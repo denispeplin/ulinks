ccb4eca1dde859bf3ec116ff7b2d50186814c151
Denis Peplin <denis.peplin@gmail.com>
2012-04-20 13:37:46 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ulinks::Application.config.secret_token = '87435583ae0321059750f5f692c09c9022eb18f7417ceef57ee4f3d3ae1e50f0ec0e5161b490bfe7ac81eff5580662faa2060652d54e65c11654988a0088f577'
