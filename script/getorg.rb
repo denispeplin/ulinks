c57bc70e439be329926f97db7e16c71bf8f60ad0
Denis Peplin <denis.peplin@gmail.com>
2012-04-24 14:58:38 +0400

    Add (incomplete) getorg script to fill up ForumLink and WatsupLink

---
+++ script/getorg.rb
@@ -0,0 +1,39 @@
+#!/usr/local/bin/ruby
+# encoding: koi8-r
+
+require 'rubygems'
+require 'mechanize'
+require 'pp'
+require 'text'
+white = Text::WhiteSimilarity.new
+require 'translit'
+
+require "./config/environment.rb"
+Rails.application.require_environment!
+config = Rails.application.config
+
+ec = Encoding::Converter.new("utf-8", "koi8-r")
+
+agent = Mechanize.new
+
+page = agent.get(config.forum_url)
+
+forum_form  = page.form('login')
+
+forum_form.user = config.forum_user
+
+forum_form.pass = config.forum_password
+
+page = agent.submit(forum_form, forum_form.buttons.first)
+
+#pp page
+
+page.links.each do |link|
+  #puts ec.convert(link.text)
+  puts link.href + ': ' + link.text
+end
+
+#examples:
+puts white.similarity('Rakovka', 'Rakovka*')
+
+puts Translit.convert('Mikhailovka')
