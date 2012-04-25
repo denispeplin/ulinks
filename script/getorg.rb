e6ba2ee7f2e542572b7598938383a553869ffcba
Denis Peplin <denis.peplin@gmail.com>
2012-04-25 15:24:00 +0400

    Populate watsup

--- script/getorg.rb
+++ script/getorg.rb
@@ -1,5 +1,4 @@
 #!/usr/local/bin/ruby
-# encoding: koi8-r
 
 require 'rubygems'
 require 'mechanize'
@@ -16,6 +15,7 @@ ec = Encoding::Converter.new("utf-8", "koi8-r")
 
 agent = Mechanize.new
 
+# FORUM LINK COLLECTION
 page = agent.get(config.forum_url)
 
 forum_form  = page.form('login')
@@ -31,13 +31,33 @@ page = agent.submit(forum_form, forum_form.buttons.first)
 page.links.each do |link|
   #puts ec.convert(link.text)
   if link.href =~ /type=view/
-    puts link.href + ': ' + link.text
+#    puts link.href + ': ' + link.text
     forumticket = ForumLink.forumticket_extract(link.href)
     ForumLink.create(path: link.href, text: link.text, forumticket: forumticket)
   end
 end
 
-#examples:
-puts white.similarity('Rakovka', 'Rakovka*')
+# WATSUP LINK COLLECTION
 
-puts Translit.convert('Mikhailovka')
+agent.auth(config.watsup_user, config.watsup_password)
+
+page = agent.get(config.watsup_url)
+page.encoding = 'windows-1251'
+
+page.search('td').each do |tds|
+  if tds.css('a')[0]
+    if (anchor = tds.css('a')[0]['href']) =~ /device\.asp/
+      linkname = tds.text.split("\n")[1]
+      linktext = tds.text.split("\n")[1].gsub(/\(.*\)$/, '').chop
+      linkip = tds.text.split("\n")[1][/.*\(([^\)]*)\)/,1]
+      puts "#{linktext} : #{linkip} : #{anchor}"
+      WatsupLink.create(path: anchor, text: linktext, ip_address: linkip)
+    end
+  end
+end
+#pp page
+
+#examples:
+#puts white.similarity('Rakovka', 'Rakovka*')
+
+#puts Translit.convert('Mikhailovka')
